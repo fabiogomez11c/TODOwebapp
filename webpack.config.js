12c9aac3d968879ce3bb363241510959ab869b48
Fabio Gomez <fabiogomez11c@gmail.com>
2021-07-30 15:25:21 -0500

    Created the webpack config, imported the assets.

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const CopyPlugin        = require('copy-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+
+    output: {
+        clean: true 
+    },
+
+    module : {
+        rules : [
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    optimization: {},
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'src/index.html'
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                {from: 'src/assets/', to: 'assets/'}
+            ]
+        })
+    ]
+}
+
+
+
