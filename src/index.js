fd51d5805a43add2ea095bc3320894384da486be
Fabio Gomez <fabiogomez11c@gmail.com>
2021-08-02 17:02:01 -0500

    Finished the first version of todo part

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ const dragMouse = (elmnt) => {
 
             // relocate the parent
             elmnt.parentElement.style.top = (elmnt.parentElement.offsetTop - pos2) + "px";
-            elmnt.parentElement.style.left = (elmnt.parentElement.offsetLeft - pos1) + "px"
+            elmnt.parentElement.style.left = (elmnt.parentElement.offsetLeft - pos1) + "px";
 
             }
 
@@ -50,17 +50,36 @@ const dragMouse = (elmnt) => {
 
 // tasks.js
 
+const todoListDiv = document.querySelector(".todo__list");
+const doneBtn     = document.querySelector(".todo__buttons--done");
+const allBtn      = document.querySelector(".todo__buttons--all");
+
 class Todo {
 
-    constructor(task){
-        this.task      = task;
+    constructor(){
         this.id        = new Date().getTime();
         this.completed = false;
+
+        this.createHTML();
     }
 
     createHTML () {
 
-        return null;
+        const htmlTodo = `
+        <div class="todo__list--li" todo-id="${this.id}">
+
+            <span>
+                <img src="/src/assets/nochecked.svg" alt="" class="todo__list--input--li-box">
+            </span>
+            <input class="todo__list--input--li-input" type="text" placeholder="Add a new task and press Enter!">
+            
+        </div>
+        `
+
+        const tempDiv = document.createElement('div');
+        tempDiv.innerHTML = htmlTodo;
+
+        todoListDiv.append(tempDiv.firstElementChild);
     }
 
 }
@@ -75,12 +94,92 @@ class TodoList {
         this.todos.push(todo);
     }
 
+    checkTodo (id) {
+
+        for (let temp_todo of this.todos) {
+
+            if (id == temp_todo.id) {
+                temp_todo.completed = !temp_todo.completed;
+            }
+        }
+    }
+
+    removeDone() {
+
+        // get the completed items
+        const temp_completed = this.todos.filter(todo => todo.completed);
+        const temp_ids       = [];
+
+        temp_completed.forEach((x) => temp_ids.push(x.id));
+
+        for (let i = todoListDiv.children.length-1; i >= 0; i--){
+            const temp_todo = todoListDiv.children[i];
+            const id_       = parseFloat(temp_todo.getAttribute("todo-id"));
+            if (temp_ids.includes(id_)){
+                todoListDiv.removeChild(temp_todo);
+            }
+        }
+    }
+
+    removeAll() {
+
+        todoListDiv.innerHTML = '';
+
+        list_object.newTodo(new Todo());
+    }
 }
 
-const newTodoHTML = () => {
+// events
+todoListDiv.addEventListener('keypress', (event) => {
+    
+    if (event.key === "Enter" && event.srcElement.value.length > 0){
+        // Create a new todo
+        list_object.newTodo(new Todo());
+        let all_inputs = document.querySelectorAll('.todo__list--input--li-input');
+        all_inputs[all_inputs.length - 1].focus();
+    }
 
-    return null;
-}
+});
+
+todoListDiv.addEventListener('click', (event) => {
+
+    const localName    = event.target.localName;
+    const targetObject = event.target;
+
+    if (localName === 'img'){
+        let targetInput = targetObject.parentElement.parentElement.querySelector("input");
+        if (targetObject.src.includes('nochecked')){
+            // check if the length > 0
+            if (targetInput.value.length > 0){
+                targetObject.src = 'assets/checked.svg';
+
+                targetInput.style['text-decoration'] = 'line-through';
+
+                // change the property of the instance
+                list_object.checkTodo(targetInput.parentElement.getAttribute('todo-id'));
+            }
+        } else if (targetObject.src.includes('checked')){
+            targetObject.src = 'assets/nochecked.svg';
+
+            targetInput.style['text-decoration'] = '';
+
+            // change the property of the instance
+            list_object.checkTodo(targetInput.parentElement.getAttribute('todo-id'));
+        }
+    }
+})
+
+doneBtn.addEventListener('click', (event) => {
+
+    list_object.removeDone();
+
+})
+
+allBtn.addEventListener('click', (event) => {
+
+    list_object.removeAll();
+
+})
 
 // -----
 
@@ -90,5 +189,6 @@ dragMouse(todoElement);
 
 // Creates a new todo at the beginning
 const list_object = new TodoList();
+list_object.newTodo(new Todo());
 
 // -----
